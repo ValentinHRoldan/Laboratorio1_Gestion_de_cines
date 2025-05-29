1fd9f6f15862d092d53e8d1b2e080862e0783ee4
valentinroldan223 <valentinroldan223@gmail.com>
2025-05-29 00:11:11 -0300

    detalles del asiento al crear una reserva

--- apps/reservas/api/serializers.py
+++ apps/reservas/api/serializers.py
@@ -1,19 +1,26 @@
 from rest_framework import serializers
-from ..models import Reserva
+from ..models import Reserva, AsientoReservado
 from django.utils import timezone
 from rest_framework.exceptions import ValidationError
 from datetime import timedelta
 from apps.funciones.models import Asiento
+from apps.funciones.api.serializers import AsientoSerializer
+
+class AsientoReservadoSerializer(serializers.ModelSerializer):
+    asiento = AsientoSerializer(read_only=True)
+    class Meta:
+        model = AsientoReservado
+        fields = ['id', 'asiento', 'funcion']
 
 class ReservaSerializer(serializers.ModelSerializer):
     asientos = serializers.ListField(
         child=serializers.IntegerField(), write_only=True
     )
-
+    asientos_reservados = AsientoReservadoSerializer(many=True, read_only=True)
     class Meta:
         model = Reserva
-        fields = ['id', 'usuario', 'funcion', 'cantidad_entradas', 'asientos']
-        read_only_fields = ['usuario']
+        fields = ['id', 'usuario', 'funcion', 'cantidad_entradas', 'asientos', 'asientos_reservados']
+        read_only_fields = ['usuario', 'asientos_reservados']
 
     def generarError(self, mensaje):
         raise serializers.ValidationError({
@@ -44,3 +51,4 @@ class ReservaSerializer(serializers.ModelSerializer):
         # Los devolvemos para que perform_create los procese
         self.context['asiento_ids'] = asiento_ids
         return reserva
+    
